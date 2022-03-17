7a1bf1c363972b2157a106eeb45319c8ec700b64
pim.vanwieren <pim.vanwieren@student.hku.nl>
2022-03-17 03:43:10 +0100

    FIrst point-and-click test

---
+++ Assets/hoverScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverScript : MonoBehaviour
+{
+    public LayerMask selectableLayer;
+    public Animator cameraAnimator;
+    private Camera camera;
+    private Animator hoverAnimator;
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, selectableLayer))
+        {
+            hoverAnimator = hit.transform.gameObject.GetComponent<Animator>();
+            hoverAnimator.SetBool("isSelected", true);
+
+            if (Input.GetMouseButtonUp(0))
+            {
+
+                cameraAnimator.SetTrigger(hit.transform.gameObject.name);
+            }
+        }
+        else
+        {
+            hoverAnimator.SetBool("isSelected", false);
+        }
+    }
+
+    public void GoBack()
+    {
+        cameraAnimator.SetTrigger("Back");
+        //ResetAllTriggers();
+    }
+
+    public void TurnLeft()
+    {
+        cameraAnimator.SetTrigger("Left");
+        //ResetAllTriggers();
+    }
+    public void Turnright()
+    {
+        cameraAnimator.SetTrigger("Right");
+        //ResetAllTriggers();
+    }
+
+    private void ResetAllTriggers()
+    {
+        foreach (var param in cameraAnimator.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Trigger)
+            {
+                cameraAnimator.ResetTrigger(param.name);
+            }
+        }
+    }
+}
+
